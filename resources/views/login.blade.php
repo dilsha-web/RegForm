468a0b2b3230f68696fded5ffc258ed26c03952e
dilsha-web <rameshi.wijesinghe@gmail.com>
2021-06-08 13:42:34 +0530

    Login form was created

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,22 @@
-<h1>Login page</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+   <h1>Login page</h1>   
+   <form action="">
+      @csrf
+      User Name
+       <input type="text" name="username"><br>
+     Password
+     <input type="password" name="password"><br>
+      
+       </form> 
+</body>
+</html>
+
+
